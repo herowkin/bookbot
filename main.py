3610fda2eee90ec980376ec056a7434e27c59690
Braiden M Jensen <braidenjensen@gmail.com>
2025-10-21 14:42:55 -0400

    added a proper main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath, "r", encoding="utf-8") as f:
+        return f.read()
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+if __name__ == "__main__":
+    main()
