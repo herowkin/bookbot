72b33bc00ebb018d69b7189335daa0e85e8bab11
Braiden M Jensen <braidenjensen@gmail.com>
2025-10-23 21:31:43 -0400

    Added stats

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_words
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -6,13 +9,8 @@ def main():
 
 
 def get_book_text(path):
-    with open(path) as f:
+       with open(path, "r", encoding="utf-8-sig") as f:
         return f.read()
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 
 main()
