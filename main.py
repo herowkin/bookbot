8a8ec7cc233e7fdb07282740a479279d11be4e05
Braiden M Jensen <braidenjensen@gmail.com>
2025-10-25 14:28:27 -0400

    added sort method

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     num_words = get_num_words(text)
     print(f"Found {num_words} total words")
     chars_dict = get_num_chars(text)
-    print(chars_dict)
+    # print(chars_dict)
+    
+    def sort_on(item):
+        return item[1]
+
+    sorted_chars = sorted(chars_dict.items(), key=sort_on, reverse=True)
+    for char, count in sorted_chars:
+        print(f"'{char}': {count}")
 
 
 def get_book_text(path):
